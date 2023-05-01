9a149cb4da907cf95361871338a06028faef9f16
Mohamed Amine Hamdouni <mohamed@mvst.co>
2023-05-02 00:39:38 +0100

    finishes http server for kafka

---
+++ kafka/src/main/java/com/example/DataIngestionAPI.java
@@ -0,0 +1,64 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.io.ByteArrayOutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class DataIngestionAPI {
+
+    private static Producer<String, String> producer;
+
+    public static void main(String[] args) throws Exception {
+        // Set up Kafka producer
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producer = new KafkaProducer<>(props);
+
+        // Set up HTTP server
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/ingest", new DataIngestionHandler());
+        server.setExecutor(Executors.newFixedThreadPool(8));
+        server.start();
+    }
+
+    static class DataIngestionHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // Get the data from the request body
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int length;
+            InputStream inputStream = exchange.getRequestBody();
+            while ((length = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, length);
+            }
+            String data = outputStream.toString("UTF-8");
+
+            // Publish to Kafka
+            ProducerRecord<String, String> record = new ProducerRecord<>("rich_people_topic", null, data);
+            producer.send(record);
+
+            // Send a response
+            String response = "Data received and sent to Kafka";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
---
+++ spark/src/main/java/com/example/DataProcessor.java
@@ -0,0 +1,105 @@
+import java.util.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
+
+public class DataProcessor {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Configure Spark
+        SparkConf conf = new SparkConf().setAppName("RichestPeopleProcessor").setMaster("local[*]");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, new Duration(5000));
+
+        // Configure Kafka
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "localhost:9092");
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", "rich_people_processor");
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+
+        // Subscribe to the Kafka topic
+        Collection<String> topics = Arrays.asList("rich_people_topic");
+        JavaDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(
+            jssc,
+            LocationStrategies.PreferConsistent(),
+            ConsumerStrategies.Subscribe(topics, kafkaParams)
+        );
+
+        // Process the data
+        JavaDStream<RichPersonData> richPeopleData = stream.map(record -> {
+            // Deserialize JSON data
+            Gson gson = new Gson();
+            return gson.fromJson(record.value(), RichPersonData.class);
+        });
+
+        // Aggregate data by country
+        JavaPairDStream<String, Tuple2<Integer, Integer>> aggregatedData = richPeopleData
+            .mapToPair(
+                (PairFunction<RichPersonData, String, Tuple2<Integer, Integer>>) richPersonData ->
+                    new Tuple2<>(
+                        richPersonData.getCountry(),
+                        new Tuple2<>(1, richPersonData.getCharacteristics().length())
+                    )
+            )
+            .reduceByKey(
+                (Function2<Tuple2<Integer, Integer>, Tuple2<Integer, Integer>, Tuple2<Integer, Integer>>) (
+                        tuple1,
+                        tuple2
+                    ) ->
+                    new Tuple2<>(tuple1._1() + tuple2._1(), tuple1._2() + tuple2._2())
+            );
+
+        // Print the aggregated data
+        aggregatedData.print();
+
+        // Start the Spark Streaming context and await termination
+        jssc.start();
+        jssc.awaitTermination();
+    }
+
+    public static class RichPersonData {
+
+        private String country;
+        private String name;
+        private String characteristics;
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCharacteristics() {
+            return characteristics;
+        }
+
+        public void setCharacteristics(String characteristics) {
+            this.characteristics = characteristics;
+        }
+    }
+}
