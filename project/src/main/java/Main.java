d2718b27a0a8d5fa557ab61953b9259ebbe06da6
Mohamed Amine Hamdouni <mohamed@mvst.co>
2023-05-06 12:33:17 +0100

    adds mongodb storing to kafka instead of hdfs

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -1,16 +1,66 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import infrastructure.CSVUtility;
+import org.bson.Document;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Map.Entry;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String csvFile = args[0];
-        List<String[]> data = CSVUtility.readCSV(csvFile);
 
-        // Get most common values for each field
-        CSVUtility.getMostCommonValues(data);
+  public static void main(String[] args) throws Exception {
+    String csvFile = args[0];
+    List<String[]> data = CSVUtility.readCSV(csvFile);
 
-        // Get most common values for each field per country
-        CSVUtility.getMostCommonValuesByCountry(data);
+    // Get most common values in general
+    Map<String, List<Entry<String, Integer>>> mostCommonValues = CSVUtility.getMostCommonValues(data);
+
+    // Get most common values by country
+    Map<String, Map<String, String>> mostCommonValuesByCountry = CSVUtility.getMostCommonValuesByCountry(
+      data
+    );
+
+    // Connect to MongoDB
+    String connectionString =
+      "mongodb+srv://dalideco:Pcqq0HU4dTCUSSt3@bigdata.pod5mkc.mongodb.net/?retryWrites=true&w=majority";
+    MongoClientSettings settings = MongoClientSettings
+      .builder()
+      .applyConnectionString(new ConnectionString(connectionString))
+      .build();
+    MongoClient mongoClient = MongoClients.create(settings);
+    MongoDatabase database = mongoClient.getDatabase("bigdata");
+
+    // Insert general most common values data into MongoDB
+    MongoCollection<Document> generalCollection = database.getCollection("common_values");
+        Document generalData = new Document();
+        for (String field : mostCommonValues.keySet()) {
+            List<Entry<String, Integer>> values = mostCommonValues.get(field);
+            generalData.put(field, values.stream()
+                .map(entry -> new Document("value", entry.getKey()).append("count", entry.getValue()))
+                .collect(Collectors.toList()));
+        }
+        generalCollection.insertOne(generalData);
+
+    // Insert most common values by country data into MongoDB
+    MongoCollection<Document> collection = database.getCollection(
+      "common_values_by_country"
+    );
+    for (String country : mostCommonValuesByCountry.keySet()) {
+      Document countryData = new Document("type", "country")
+        .append("country", country);
+      Map<String, String> fields = mostCommonValuesByCountry.get(country);
+      for (String field : fields.keySet()) {
+        countryData.put(field, fields.get(field));
+      }
+      collection.insertOne(countryData);
     }
+
+    mongoClient.close();
+  }
 }
--- project/src/main/java/infrastructure/CSVUtility.java
+++ project/src/main/java/infrastructure/CSVUtility.java
@@ -1,139 +1,154 @@
 package infrastructure;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-
-import com.opencsv.exceptions.CsvException;
-import com.opencsv.CSVReader;
 import utils.FileHandler;
 
 public class CSVUtility {
-    public static final int COUNTRY_INDEX = 6;
-    public static final String[] HEADERS = { "rank", "personName", "age", "finalWorth", "category", "source", "country",
-            "state", "city", "organization", "selfMade", "gender", "birthDate", "title", "philanthropyScore", "bio",
-            "about" };
 
-    public static List<String[]> readCSV(String filePath) throws IOException, CsvException {
-        CSVReader reader = new CSVReader(new FileReader(filePath));
-        List<String[]> data = reader.readAll();
-        reader.close();
+  public static final int COUNTRY_INDEX = 6;
+  public static final String[] HEADERS = {
+    "rank",
+    "personName",
+    "age",
+    "finalWorth",
+    "category",
+    "source",
+    "country",
+    "state",
+    "city",
+    "organization",
+    "selfMade",
+    "gender",
+    "birthDate",
+    "title",
+    "philanthropyScore",
+    "bio",
+    "about",
+  };
 
-        return data;
+  public static List<String[]> readCSV(String filePath)
+    throws IOException, CsvException {
+    CSVReader reader = new CSVReader(new FileReader(filePath));
+    List<String[]> data = reader.readAll();
+    reader.close();
+
+    return data;
+  }
+
+  public static Map<String, List<Map.Entry<String, Integer>>> getMostCommonValues(
+    List<String[]> data
+  ) {
+    Map<String, Map<String, Integer>> dataByField = groupDataByField(data);
+    Map<String, List<Map.Entry<String, Integer>>> mostCommonValues = new HashMap<>();
+
+    for (String field : dataByField.keySet()) {
+      Map<String, Integer> valueCounts = dataByField.get(field);
+      if (!valueCounts.isEmpty()) {
+        List<Map.Entry<String, Integer>> sortedValueCounts = getSortedEntries(
+          valueCounts
+        );
+        mostCommonValues.put(
+          field,
+          sortedValueCounts.subList(0, Math.min(10, sortedValueCounts.size()))
+        );
+      }
     }
 
-    public static void getMostCommonValues(List<String[]> data) {
+    return mostCommonValues;
+  }
 
-        FileHandler fileHandler = new FileHandler("output/common_values.txt");
-        if (!fileHandler.isValid())
-            return;
+  public static Map<String, Map<String, String>> getMostCommonValuesByCountry(
+    List<String[]> data
+  ) {
+    Map<String, Map<String, Map<String, Integer>>> dataByCountryAndField = groupDataByCountryAndField(
+      data
+    );
+    Map<String, Map<String, String>> mostCommonValuesByCountry = new HashMap<>();
 
-        fileHandler.loadWriter();
+    for (String country : dataByCountryAndField.keySet()) {
+      mostCommonValuesByCountry.put(country, new HashMap<>());
+      Map<String, Map<String, Integer>> dataByField = dataByCountryAndField.get(
+        country
+      );
+      for (String field : dataByField.keySet()) {
+        Map<String, Integer> valueCounts = dataByField.get(field);
+        if (!valueCounts.isEmpty()) {
+          String mostCommonValue = "";
+          int mostCommonValueCount = 0;
+          for (Map.Entry<String, Integer> entry : valueCounts.entrySet()) {
+            String value = entry.getKey();
+            int count = entry.getValue();
+            if (count > mostCommonValueCount) {
+              mostCommonValue = value;
+              mostCommonValueCount = count;
+            }
+          }
+          mostCommonValuesByCountry.get(country).put(field, mostCommonValue);
+        }
+      }
+    }
+    return mostCommonValuesByCountry;
+  }
 
-        Map<String, Map<String, Integer>> dataByField = groupDataByField(data);
-        for (String field : dataByField.keySet()) {
-            fileHandler.write(field + ": \n");
+  public static Map<String, Map<String, Integer>> groupDataByField(
+    List<String[]> data
+  ) {
+    Map<String, Map<String, Integer>> dataByField = new HashMap<>();
+    for (String[] row : data) {
+      for (int i = 2; i < HEADERS.length - 2; i++) {
+        String field = HEADERS[i];
+        String value = row[i];
+        if (!dataByField.containsKey(field)) {
+          dataByField.put(field, new HashMap<>());
+        }
+        if (!value.isEmpty()) {
+          Map<String, Integer> valueCounts = dataByField.get(field);
+          valueCounts.put(value, valueCounts.getOrDefault(value, 0) + 1);
+        }
+      }
+    }
+    return dataByField;
+  }
+
+  public static Map<String, Map<String, Map<String, Integer>>> groupDataByCountryAndField(
+    List<String[]> data
+  ) {
+    Map<String, Map<String, Map<String, Integer>>> dataByCountryAndField = new HashMap<>();
+    for (String[] row : data) {
+      String country = row[COUNTRY_INDEX];
+      if (!dataByCountryAndField.containsKey(country)) {
+        dataByCountryAndField.put(country, new HashMap<>());
+      }
+      Map<String, Map<String, Integer>> dataByField = dataByCountryAndField.get(
+        country
+      );
+      for (int i = 2; i < HEADERS.length - 2; i++) {
+        if (i != COUNTRY_INDEX) {
+          System.out.println("header " + HEADERS[i] + i);
+          String field = HEADERS[i];
+          String value = row[i];
+          if (!dataByField.containsKey(field)) {
+            dataByField.put(field, new HashMap<>());
+          }
+          if (!value.isEmpty()) {
             Map<String, Integer> valueCounts = dataByField.get(field);
-            if (valueCounts.isEmpty()) {
-                fileHandler.write("- No data available \n");
-            } else {
-                List<Map.Entry<String, Integer>> sortedValueCounts = getSortedEntries(valueCounts);
-                for (int i = 0; i < Math.min(10, sortedValueCounts.size()); i++) {
-                    Map.Entry<String, Integer> entry = sortedValueCounts.get(i);
-                    String value = entry.getKey();
-                    int count = entry.getValue();
-                    fileHandler.write("- " + value + " (count: " + count + ") \n");
-                }
-            }
+            valueCounts.put(value, valueCounts.getOrDefault(value, 0) + 1);
+          }
         }
-
-        fileHandler.closeWriter();
+      }
     }
+    return dataByCountryAndField;
+  }
 
-    public static void getMostCommonValuesByCountry(List<String[]> data) {
-        FileHandler fileHandler = new FileHandler("output/common_values_by_country.txt");
-        if (!fileHandler.isValid())
-            return;
-
-        fileHandler.loadWriter();
-
-        Map<String, Map<String, Map<String, Integer>>> dataByCountryAndField = groupDataByCountryAndField(data,
-                fileHandler);
-        for (String country : dataByCountryAndField.keySet()) {
-            fileHandler.write("Country: " + country + " \n");
-            Map<String, Map<String, Integer>> dataByField = dataByCountryAndField.get(country);
-            for (String field : dataByField.keySet()) {
-                fileHandler.write(field + ": \n");
-                Map<String, Integer> valueCounts = dataByField.get(field);
-                if (valueCounts.isEmpty()) {
-                    fileHandler.write("- No data available \n");
-                } else {
-                    String mostCommonValue = "";
-                    int mostCommonValueCount = 0;
-                    for (Map.Entry<String, Integer> entry : valueCounts.entrySet()) {
-                        String value = entry.getKey();
-                        int count = entry.getValue();
-                        if (count > mostCommonValueCount) {
-                            mostCommonValue = value;
-                            mostCommonValueCount = count;
-                        }
-                    }
-                    fileHandler.write("- " + mostCommonValue + " (count: " + mostCommonValueCount + ") \n");
-                }
-            }
-        }
-        fileHandler.closeWriter();
-    }
-
-    public static Map<String, Map<String, Integer>> groupDataByField(List<String[]> data) {
-        Map<String, Map<String, Integer>> dataByField = new HashMap<>();
-        for (String[] row : data) {
-            for (int i = 2; i < HEADERS.length - 2; i++) {
-                String field = HEADERS[i];
-                String value = row[i];
-                if (!dataByField.containsKey(field)) {
-                    dataByField.put(field, new HashMap<>());
-                }
-                if (!value.isEmpty()) {
-                    Map<String, Integer> valueCounts = dataByField.get(field);
-                    valueCounts.put(value, valueCounts.getOrDefault(value, 0) + 1);
-                }
-            }
-        }
-        return dataByField;
-    }
-
-    public static Map<String, Map<String, Map<String, Integer>>> groupDataByCountryAndField(List<String[]> data,
-            FileHandler fileHandler) {
-        Map<String, Map<String, Map<String, Integer>>> dataByCountryAndField = new HashMap<>();
-        for (String[] row : data) {
-            String country = row[COUNTRY_INDEX];
-            if (!dataByCountryAndField.containsKey(country)) {
-                dataByCountryAndField.put(country, new HashMap<>());
-            }
-            Map<String, Map<String, Integer>> dataByField = dataByCountryAndField.get(country);
-            for (int i = 2; i < HEADERS.length - 2; i++) {
-                if (i != COUNTRY_INDEX) {
-                    System.out.println("header " + HEADERS[i] + i);
-                    String field = HEADERS[i];
-                    String value = row[i];
-                    if (!dataByField.containsKey(field)) {
-                        dataByField.put(field, new HashMap<>());
-                    }
-                    if (!value.isEmpty()) {
-                        Map<String, Integer> valueCounts = dataByField.get(field);
-                        valueCounts.put(value, valueCounts.getOrDefault(value, 0) + 1);
-                    }
-
-                }
-            }
-        }
-        return dataByCountryAndField;
-    }
-
-    public static List<Map.Entry<String, Integer>> getSortedEntries(Map<String, Integer> map) {
-        List<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
-        entries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        return entries;
-    }
+  public static List<Map.Entry<String, Integer>> getSortedEntries(
+    Map<String, Integer> map
+  ) {
+    List<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
+    entries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+    return entries;
+  }
 }
